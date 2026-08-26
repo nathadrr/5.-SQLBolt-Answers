1a8609b8a6cd0c87eef1215bbb747d9211228f5a
Nathan Adrian Chandra <nathadrr@gmail.com>
2026-08-26 17:37:14 +0700

    Add files via upload

---
+++ ex1.sql
@@ -0,0 +1,15 @@
+-- Exsercise 1
+--1.Find the title of each film 
+SELECT Title FROM movies;
+
+--2.Find the director of each film
+SELECT Director FROM movies;
+
+--3.Find the title and director of each film
+SELECT Title, Director FROM movies;
+
+--4.Find the title and year of each film
+SELECT Title, year FROM movies;
+
+--5.Find all the information about each film
+SELECT * FROM movies;
